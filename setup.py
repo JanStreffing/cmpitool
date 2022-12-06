0532793ebf140ddf9bf07578a4fcce12302aee72
Jan Streffing <jan.streffing@awi.de>
2022-12-06 16:01:16 +0100

    update tar download path after the fact

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/JanStreffing/cmpitool",
-    download_url="https://github.com/JanStreffing/cmpitool/archive/0.1.0.tar.gz",
+    download_url="https://github.com/JanStreffing/cmpitool/archive/refs/tags/v0.1.0.tar.gz",
     version="0.1.0",
     zip_safe=False,
 )
