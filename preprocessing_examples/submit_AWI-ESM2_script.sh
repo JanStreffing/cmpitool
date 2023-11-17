1fcbcad7cd6d0addf825e447e0700867a7688f77
Jan Streffing <jan.streffing@awi.de>
2023-11-17 13:34:25 +0100

    adding submission script

---
+++ preprocessing_examples/submit_AWI-ESM2_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=noncmore_preprocess_AWI-ESM2
+#SBATCH --partition=smp
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=04:30:00
+#SBATCH --qos=12h
+#SBATCH --mail-type=FAIL
+#SBATCH --account=paleodyn.paleodyn
+#SBATCH --output=noncmore_preprocess_AWI-ESM2.%j.out
+#SBATCH --mem=6000MB
+
+# limit stacksize ... adjust to your programs need
+# and core file size
+#ulimit -s 204800
+#ulimit -c 0
+
+export PATH="/work/ab0246/HPC_libraries/intel-oneapi-compilers/2022.0.1-gcc-11.2.0/openmpi/4.1.2-gcc-11.2.0/bin/:${PATH}"
+
+set -e
+
+./noncmore_preprocess_AWI-ESM2.sh /work/bb1029/a270107/esm_tools/awiesm-2.1-wiso/EXP/PI_CTRL/outdata/ /home/a/a270061/cmpitool/cmpitool/input/ AWI-ESM2 0 4975 4999 /home/a/a270061/cmpitool/cmpitool/grid.nc /home/a/a270061/cmpitool/cmpitool/input/tmpdata /home/a/a270061/cmpitool/cmpitool/nz1_variables.dump /home/a/a270061/cmpitool/cmpitool/nz1_data.dump "1"
