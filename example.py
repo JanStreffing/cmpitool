54e5ae29248675a22506f704794cd40208050e82
Jan Streffing <jan.streffing@awi.de>
2024-04-02 17:10:10 +0200

    add back in second example model

--- example.py
+++ example.py
@@ -9,6 +9,7 @@ variable, region, climate_model, siconc, tas, clt, pr, rlut, uas, vas, ua, zg, z
 model_path='/work/ab0246/a270092/postprocessing/cmip6_cmpitool/'
 models=[    
         climate_model(name='AWI-CM-1-1-MR',variables=[        tas, clt, pr, rlut, uas, vas, ua, zg, zos, tos, mlotst, thetao, so]),
+        climate_model(name='EC-Earth3',    variables=[siconc, tas, clt, pr, rlut, uas, vas, ua, zg, zos, tos, mlotst, thetao, so]),
     ]
 
 cmpitool(model_path, models, verbose=True, biasmaps=True)
